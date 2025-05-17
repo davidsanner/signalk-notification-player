8c9e32d37fb3d53b1acc21ac0d94f40d6d50d3b0
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2025-05-17 22:59:24 +0300

    feat: add 'sudo apt update' before install

--- index.js
+++ index.js
@@ -54,6 +54,11 @@ module.exports = function (app) {
 
     if (process.platform === 'linux') {
       if (pluginProps.installAudioPkgs) {
+        try {
+          execSync('sudo apt update', {stdio: 'inherit'});
+        } catch (e) {
+          console.error(`${plugin.id}: Failed to run 'sudo apt update'. Please update your package lists manually.`);
+        }
         const pkgs = ['festival', 'mpg123'];
         pkgs.forEach(pkg => {
           try {
